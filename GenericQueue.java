1b669c6827b1b179af8c481da3965fc045b69197
githubganesh2005 <168344926+githubganesh2005@users.noreply.github.com>
2024-08-24 19:37:16 +0530

    Add files via upload

---
+++ GenericQueue.java
@@ -0,0 +1,18 @@
+package teststackqueue;
+ public class GenericQueue<E> {
+  private final java.util.LinkedList<E> list 
+    = new java.util.LinkedList<>();
+  public void enqueue(E e) {
+    list.addLast(e);
+  }
+  public E dequeue() {
+    return list.removeFirst();
+  }
+  public int getSize() {
+    return list.size();
+  }
+  @Override
+  public String toString() {
+    return "Queue: " + list.toString();
+  }
+ }
